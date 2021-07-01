a5dfcf5921468a2d5302464134a845f1b3705543
dee2018 <deekurmi2018@gmail.com>
2021-07-01 15:49:30 +0530

    some changes are done

--- connection.php
+++ connection.php
@@ -1,5 +1,6 @@
 <?php
-// $con = mysqli_connect('localhost','root','','library');
-// $con = mysqli_connect('remotemysql.com','v6yZKCn7JU','a0Id4AtCaE','v6yZKCn7JU');
-$con = mysqli_connect('remotemysql.com','x6M3njj5Et','v06xdlzG0C','x6M3njj5Et');
+//$con = mysqli_connect('localhost','root','','library');
+ //$con = mysqli_connect('remotemysql.com','v6yZKCn7JU','a0Id4AtCaE','v6yZKCn7JU');
+$con = mysqli_connect('remotemysql.com','U7hNDwbgMb','HcXahSbzcj','U7hNDwbgMb');
+
 ?>
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # d-library
 
 <h3>Live Demo :- <a href="https://d-library.herokuapp.com/index.php">d-library</a></h3>
+<p>
+ClientSide    :-  HTML, CSS, Bootstrap, Javascript, jquery </br>
+ServerSide   :-  PHP </br>
+Database     :-  Mysql </br>
+ </p>
