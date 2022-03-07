fd443ead63eb48b964ca3286d622d25288daae41
Deepesh Kurmi <69786385+Deepesh-IIITN@users.noreply.github.com>
2022-03-07 23:15:02 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 //$con = mysqli_connect('localhost','root','','library');
  //$con = mysqli_connect('remotemysql.com','v6yZKCn7JU','a0Id4AtCaE','v6yZKCn7JU');
-$con = mysqli_connect('remotemysql.com','U7hNDwbgMb','HcXahSbzcj','U7hNDwbgMb');
+$con = mysqli_connect('sql6.freemysqlhosting.net','sql6477469','JkUxXzgFRI','sql6477469');
 
 ?>
